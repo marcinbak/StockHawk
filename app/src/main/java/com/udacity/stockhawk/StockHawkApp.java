ca5bc0c5d019b0e1f0ab6cb6fd792be549527d81
marcin_bak <marcin.bak@neofonie.de>
2017-01-21 23:16:19 +0100

    formatting, crash fixed

--- app/src/main/java/com/udacity/stockhawk/StockHawkApp.java
+++ app/src/main/java/com/udacity/stockhawk/StockHawkApp.java
@@ -1,18 +1,17 @@
 package com.udacity.stockhawk;
 
 import android.app.Application;
-
 import timber.log.Timber;
 
 public class StockHawkApp extends Application {
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
+  @Override
+  public void onCreate() {
+    super.onCreate();
 
-        if (BuildConfig.DEBUG) {
-            Timber.uprootAll();
-            Timber.plant(new Timber.DebugTree());
-        }
+    if (BuildConfig.DEBUG) {
+      Timber.uprootAll();
+      Timber.plant(new Timber.DebugTree());
     }
+  }
 }
